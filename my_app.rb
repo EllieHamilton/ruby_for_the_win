9988a8410b6781630a41a3b6c6123aebf56bd2ec
Kate Moksina <katemoksina@Kates-MacBook-Pro.local>
2017-09-18 15:48:33 +0100

    ping ponging

--- my_app.rb
+++ my_app.rb
@@ -2,7 +2,15 @@ require 'sinatra/base'
 
 # Our simple app
 class MyApp < Sinatra::Base
+  configure :production, :development do
+    enable :logging
+  end
+
   get '/' do
-  	'text'
+    logger.info 'route to index'
+  end
+
+  get '/ping' do
+  	'pong'
   end
 end
